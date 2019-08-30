15ad1ff2c197f0a008f74bf5b15cace31f4cbff6
BorkStick <4ciiDicBa55@gmail.com>
2019-08-30 08:08:35 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Contacts from "./components/contacts/Contacts";
+import AddContact from "./components/contacts/AddContact";
+import Header from "./components/layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Provider } from "./context";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider>
+      <div className="App">
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <AddContact />
+          <Contacts />
+        </div>
+      </div>
+    </Provider>
   );
 }
 
