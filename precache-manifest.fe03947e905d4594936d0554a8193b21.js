66cdf2383a3b4b8a7c15ba53768db89460a03215
BorkStick <4ciiDicBa55@gmail.com>
2019-08-30 10:26:57 -0500

    Updates

---
+++ precache-manifest.fe03947e905d4594936d0554a8193b21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03fecaa945f547db6a5df8ec88b72de",
+    "url": "/contact-manager-react/index.html"
+  },
+  {
+    "revision": "2fc1fd487acb9988bfa5",
+    "url": "/contact-manager-react/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3971a712e44a40af0d6f",
+    "url": "/contact-manager-react/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2fc1fd487acb9988bfa5",
+    "url": "/contact-manager-react/static/js/2.acf3b64a.chunk.js"
+  },
+  {
+    "revision": "3971a712e44a40af0d6f",
+    "url": "/contact-manager-react/static/js/main.3c0e51d1.chunk.js"
+  },
+  {
+    "revision": "3bd575c08d664680138b",
+    "url": "/contact-manager-react/static/js/runtime~main.65099a5d.js"
+  }
+]);
