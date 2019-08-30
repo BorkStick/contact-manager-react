a519631ced7a522bb6171d762e1490f9b8462937
BorkStick <4ciiDicBa55@gmail.com>
2019-08-30 09:10:14 -0500

    got axios working for jsonplaceholder

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Header = props => {
   const { branding } = props;
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+    <nav className="navbar navbar-expand-sm navbar-dark bg-success mb-3 py-0">
       <div className="container">
         <a href="/" className="navbar-brand">
           {branding}
@@ -13,20 +13,17 @@ const Header = props => {
         <ul className="navbar-nav">
           <li className="nav-item">
             <Link to="/" className="nav-link">
-              <i className="fas fa-home"></i>
-              Home
+              <i className="fas fa-home"></i> Home
             </Link>
           </li>
           <li className="nav-item">
             <Link to="/contact/add" className="nav-link">
-              <i className="fas fa-plus"></i>
-              Add
+              <i className="fas fa-plus"></i> Add
             </Link>
           </li>
           <li className="nav-item">
             <Link to="/about" className="nav-link">
-              <i className="fas fa-question"></i>
-              About
+              <i className="fas fa-question"></i> About
             </Link>
           </li>
         </ul>
